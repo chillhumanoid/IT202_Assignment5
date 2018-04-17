a6d25e3dab35b1f5cd9990458cb21130c713e1eb
Jonathan Thorne <jonathanthorne13@gmail.com>
2018-04-16 22:20:27 -0400

    fixed the problem where it would redirect to login no matter what

--- include/header.php
+++ include/header.php
@@ -1,6 +1,6 @@
 <?php
   session_start();
-  if(!isset($_SESSION[id])){
+  if(!isset($_SESSION['username'])){
     header('Location: https://web.njit.edu/~jgt8/Assignment5/login.php');
     exit;
   }
