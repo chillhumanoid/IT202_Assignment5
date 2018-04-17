f49bc7230b313b1ec94666d41e15868af201cabe
Jonathan Thorne <jonathanthorne13@gmail.com>
2018-04-17 12:30:01 -0400

    added better sterilization

--- uploadChat.php
+++ uploadChat.php
@@ -3,7 +3,7 @@
   include("include/config.php");
   if(isset($_POST['user']) && isset($_POST['message'])){
     $user = $_POST['user'];
-    $message = $_POST['message'];
+    $message = mysqli_real_escape_string($db,$_POST['message']);
     $sql = "UPDATE chat SET message = '$message' WHERE username='$user'";
     if(mysqli_query($db, $sql)){
 
