814cdc89c6b7d87667144676e3fe943c4af4719f
Jonathan Thorne <jonathanthorne13@gmail.com>
2018-04-17 12:30:18 -0400

    Accidentally messed somethings up, this fixes it.

--- checkulogin.php
+++ checkulogin.php
@@ -1,10 +1,9 @@
 <?php
   session_start();
   include("include/config.php");
-  if(isset($_POST['user']) && isset($_POST['password'])){
+  if(isset($_POST['user'])){
     $user = $_POST['user'];
-    $password = $_POST['password'];
-    $sql = "SELECT * FROM loginChat where username = '$user' AND password = '$password'";
+    $sql = "SELECT * FROM loginChat where username = '$user'";
     $result = mysqli_query($db, $sql);
     if($result->num_rows == 1){
       echo 'YES';
