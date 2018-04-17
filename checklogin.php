59cee61b7e062c8cdd494f2130ad04f029495526
Jonathan Thorne <jonathanthorne13@gmail.com>
2018-04-17 09:54:43 -0400

    check log in when in chat, strictly for grade

---
+++ checklogin.php
@@ -0,0 +1,13 @@
+<?php
+  include("include/config.php");
+  if(isset($_POST['username']) && isset($_POST['pass'])){
+    $username = $_POST['username'];
+    $pass = $_POST['pass'];
+    $sql = "SELECT * FROM loginChat where username = '$username' AND password = '$pass'";
+    $result = mysqli_query($sql);
+    if(mysqli_num_rows($result)){
+      echo 'YES';
+    }else{
+      echo 'NO';
+    }
+  }
